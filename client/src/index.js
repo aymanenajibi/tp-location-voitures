7d12f7d018ae94041aef5fe31c61125b72f377e7
Aymane <aymanee.najibi@gmail.com>
2025-02-06 16:54:58 +0100

    Systeme de Gestion

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // ✅ Import correct pour React 18
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./efm-fes-meknes/App";
+import App from "./systeme-gestion/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ Utilisation de createRoot
 root.render(
---
+++ client/src/systeme-gestion/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ListeVoitures from "./components/ListeVoitures";
+import "./style/App.css";
+
+function App() {
+  return (
+    <div>
+      <ListeVoitures />
+    </div>
+  );
+}
+
+export default App;
